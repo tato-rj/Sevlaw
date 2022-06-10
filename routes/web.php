bdad6ebd4c809c01ef4d6fabe3c19d754e7ac5a3
Arthur Villar <arthur@iMac.local>
2022-06-10 10:57:36 -0400

    Temp site ready

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('dev', function() {
+Route::get('', function() {
     return view('dev');
 })->name('dev');
 
@@ -23,28 +23,28 @@ Route::get('go', function() {
     return redirect(route('principal'));
 })->name('dev.go');
 
-Route::get('/', function () {
-    $testimonials = (new \App\Testimonials)->all();
+// Route::get('/', function () {
+//     $testimonials = (new \App\Testimonials)->all();
 
-    return view('principal.index', compact('testimonials'));
-})->name('principal');
+//     return view('principal.index', compact('testimonials'));
+// })->name('principal');
 
-Route::get('socios', function () {
-    return view('socios.index');
-})->name('socios');
+// Route::get('socios', function () {
+//     return view('socios.index');
+// })->name('socios');
 
-Route::prefix('praticas')->name('praticas.')->group(function() {
+// Route::prefix('praticas')->name('praticas.')->group(function() {
 
-    Route::get('processos-juridicos', function () {
-        return view('praticas.processos.index');
-    })->name('processos'); 
-    Route::get('assessoria-e-consultoria', function () {
-        return view('praticas.assessoria.index');
-    })->name('assessoria'); 
-    Route::get('planejamento-de-negocios', function () {
-        return view('praticas.planejamento.index');
-    })->name('planejamento');     
-    Route::get('family-business-law', function () {
-        return view('praticas.family.index');
-    })->name('family'); 
-});
+//     Route::get('processos-juridicos', function () {
+//         return view('praticas.processos.index');
+//     })->name('processos'); 
+//     Route::get('assessoria-e-consultoria', function () {
+//         return view('praticas.assessoria.index');
+//     })->name('assessoria'); 
+//     Route::get('planejamento-de-negocios', function () {
+//         return view('praticas.planejamento.index');
+//     })->name('planejamento');     
+//     Route::get('family-business-law', function () {
+//         return view('praticas.family.index');
+//     })->name('family'); 
+// });
