b339cb29d3873462aeb5cbbcb096b53ce079a271
Arthur Villar <arthur@iMac.local>
2022-08-03 20:10:52 -0400

    Fix logo

--- resources/views/layouts/components/logo.blade.php
+++ resources/views/layouts/components/logo.blade.php
@@ -1,4 +1,4 @@
     <a class="navbar-brand ml-3" href="{{route('principal')}}">
-        <img class="d-none {{$main ? 'd-sm-block' : null}} position-absolute logo-lg ml-4" src="{{asset('images/brand/logo-sm.svg')}}" style="width: 102px; top: 16px">
-        <img class="{{$main ? 'd-sm-none' : null}} ml-3 logo-sm" src="{{asset('images/brand/logo-sm.svg')}}" style="width: 72px">
+        <img class="d-none {{true ? 'd-sm-block' : null}} position-absolute logo-lg ml-4" src="{{asset('images/brand/logo-full.svg')}}" style="width: 316px; top: 10px">
+        <img class="{{true ? 'd-sm-none' : null}} ml-3 logo-sm" src="{{asset('images/brand/logo-sm.svg')}}" style="width: 72px">
     </a>
--- resources/views/principal/index.blade.php
+++ resources/views/principal/index.blade.php
@@ -82,26 +82,26 @@ $('#switch-bg').click(function() {
 });
 </script>
 
-    <script type="text/javascript">
-        var firstScreen = $('#lead-bg').offset().top + $('#lead-bg').height();
-        var $menu = $('nav.navbar')
-                        .clone()
-                        .hide()
-                        .appendTo('body')
-                        .addClass('navbar-fixed shadow bg-primary animate__fadeInDown')
-                        .removeClass('position-absolute');
+<script type="text/javascript">
+    var firstScreen = $('#lead-bg').offset().top + $('#lead-bg').height();
+    var $menu = $('nav.navbar')
+                    .clone()
+                    .hide()
+                    .appendTo('body')
+                    .addClass('navbar-fixed shadow bg-primary animate__fadeInDown')
+                    .removeClass('position-absolute');
 
-        $('nav.navbar-fixed .navbar-brand img.logo-lg').remove();
-        $('nav.navbar-fixed .navbar-brand img.logo-sm').removeClass('d-sm-none');
+    // $('nav.navbar-fixed .navbar-brand img.logo-lg').remove();
+    // $('nav.navbar-fixed .navbar-brand img.logo-sm').removeClass('d-sm-none');
 
-        $(window).scroll(function() {
-            let scrollTop = $(this).scrollTop();
+    $(window).scroll(function() {
+        let scrollTop = $(this).scrollTop();
 
-            if (scrollTop > firstScreen) {
-                $menu.show();
-            } else {
-                $menu.fadeOut('fast');
-            }
-        });
-    </script>
+        if (scrollTop > firstScreen) {
+            $menu.show();
+        } else {
+            $menu.fadeOut('fast');
+        }
+    });
+</script>
 @endpush
